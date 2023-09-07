a0926761406c4194d91af7b944b85fa542e4877a
uhwang <43251090+uhwang@users.noreply.github.com>
2023-09-07 13:23:34 -0400

    Add files via upload

--- ocr.py
+++ ocr.py
@@ -1,6 +1,11 @@
 '''
+09/06/2023
 
-python ocr.py book/crop jpg book/crop ./all-book.pdf
+python ocr.py input-path input-extension output-path output-pdf
+
+Ex: 
+python ocr.py d:/ocr jpg d:/ocr/pdf d:/ocr/pdf/book.pdf
+python ocr.py d:/ocr/pdf pdf d:/ocr book.pdf --merge-only
 
 '''
 from PIL import Image
@@ -9,14 +14,16 @@ import pathlib
 import pytesseract as ocr
 from pypdf import PdfMerger
 
+# change the path if necessary
 ocr.pytesseract.tesseract_cmd = r'C:\\Tesseract-OCR\\tesseract.exe'
 
 def print_usage():
-    print("python [path1] [ext1] [path2] [ofile]\n"
+    print("python [path1] [ext1] [path2] [ofile] [--merge-only]\n"
           "path1 : input folder\n"
-          "ext1  : the extension of input files(jpg or png)\n"
+          "ext1  : the extension of input files(jpg or png or pdf)\n"
           "path2 : subfolder name under path1\n"
           "ofile : output file(ex: aaa.pdf\n"
+          "--merge-only: merge multiple pdfs(ext must be pdf)"
           "Ex) python aaa\bbb png ./ book.pdf")
 
 def save(options):
@@ -45,30 +52,34 @@ def save(options):
             
     print("=> Success")
     
-    pdf_list = []
-    
-    for i_, f_ in enumerate(files):
-        #f_in = pathlib.Path.joinpath(path1, f_)
-        f_in = f_
-        f_out= pathlib.Path.joinpath(path2, "%s.pdf"%f_.stem)
-        print("%s --> %s"%(str(f_in),str(f_out)))
+    if "--merge-only" in options:
+        pdf_list = files
+    else:
         
-        try:
-            pdf = ocr.image_to_pdf_or_hocr(str(f_in), extension='pdf')
-            with open(str(f_out), 'w+b') as f:
-                f.write(pdf) # pdf type is bytes by default
-        except Exception as e:
-            print("... Error: %s"%e)
-            return
-        pdf_list.append(f_out)
-        #print("=> Converting %d"%i_)
+        pdf_list = []
+        
+        for i_, f_ in enumerate(files):
+            #f_in = pathlib.Path.joinpath(path1, f_)
+            f_in = f_
+            f_out= pathlib.Path.joinpath(path2, "%s.pdf"%f_.stem)
+            print("%s --> %s"%(str(f_in),str(f_out)))
+            
+            try:
+                pdf = ocr.image_to_pdf_or_hocr(str(f_in), extension='pdf')
+                with open(str(f_out), 'w+b') as f:
+                    f.write(pdf) # pdf type is bytes by default
+            except Exception as e:
+                print("... Error: %s"%e)
+                return
+            pdf_list.append(f_out)
+            #print("=> Converting %d"%i_)
             
     merger = PdfMerger()
 
-    for pdf in pdf_list:
-        merger.append(str(pdf))
+    for p in pdf_list:
+        merger.append(str(p))
     
-    pdf_final = pathlib.Path.joinpath(path2, options[3])
+    pdf_final = pathlib.Path(options[3])
     merger.write(str(pdf_final))
     merger.close()
     
